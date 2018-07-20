da7f7876da17fa157294256106b7173cc3ad905f
bsaborio <bsaborio@isppro.net>
2018-07-20 15:43:11 -0600

    Update test

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'pm-root',
   //templateUrl: './app.component.html'
-  template: `<h1>{{pageTitle}}</h1>
-            <pm-products></pm-products>`
+  template: `
+  <a class='navbar-brand'>{{pageTitle}}</a>
+  <ul class='nav nav-pills'>
+    <li><a routerLink='active' [routerLink]="['/welcome']">Home</a></li>
+    <li><a routerLink='active' [routerLink]="['/products']">Product List</a></li>
+  </ul>`
 })
 export class AppComponent {
   pageTitle: string = 'Angular: Getting Started T Test';
